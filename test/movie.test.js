c0bf671fce91d1b29d9c76d66848684b7cd40bd7
Alertis <osmanyavuzdemir96@gmail.com>
2019-01-19 13:26:04 +0300

    movie test complated

---
+++ test/movie.test.js
@@ -0,0 +1,36 @@
+const chai=require('chai');
+const chaiHttp=require('chai-http');
+const should=chai.should();
+const server=require('../app');
+
+chai.use(chaiHttp);
+
+let token;
+
+describe('/api/movies Test', ()=>{
+    before((done)=>{
+        chai.request(server)
+        .post('/authenticate')
+        .send({username:'Alertis',password:'Alertis'})
+        .end((err,res)=>{
+            if(err)
+                throw err;
+            token=res.body.token;
+            done();
+        });
+    });
+
+    describe('/GET movies',()=>{
+        it('it should get all the movies',(done)=>{
+            chai.request(server)
+                .get('/api/movies')
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+});
+
