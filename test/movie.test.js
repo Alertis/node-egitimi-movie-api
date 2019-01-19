40d634e2d0ce8738a40dc419f8356a807dc6f79b
Alertis <osmanyavuzdemir96@gmail.com>
2019-01-19 21:37:31 +0300

    movie test completed (GET POST PUT DELETE)

--- test/movie.test.js
+++ test/movie.test.js
@@ -5,7 +5,7 @@ const server=require('../app');
 
 chai.use(chaiHttp);
 
-let token;
+let token,movie_id;
 
 describe('/api/movies Test', ()=>{
     before((done)=>{
@@ -28,6 +28,85 @@ describe('/api/movies Test', ()=>{
                 .end((err,res)=>{
                     res.should.have.status(200);
                     res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+
+    describe('/POST movie',()=>{
+        it('it should get a movie',(done)=>{
+            const movie={
+                title:'Udemy',
+                director_id:'5c41d3fa9aca0f17ce1277dd',
+                category:'Test',
+                country:'Turkey',
+                year:1950,
+                imdb_score:8
+            }
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    movie_id=res.body._id;
+                    done();
+                });
+        });
+    });
+
+    describe('/GET/:movie_id movie',()=>{
+        it('it should get a movie by the given id',(done)=>{
+            chai.request(server)
+                .get('/api/movies/'+movie_id)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('_id').eql(movie_id);
+                    done();
+                });
+        });
+    });
+
+    describe('/PUT/:movie_id movie',()=>{
+        it('it should update a movie given by id',(done)=>{
+            const movie={
+                title:'UpdateTest',
+                director_id:'5c41d3fa9aca0f17ce1277d7',
+                category:'Update',
+                country:'China',
+                year:1970,
+                imdb_score:6
+            }
+            chai.request(server)
+                .put('/api/movies/'+movie_id)
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                    done();
+                });
+        });
+    });
+
+    describe('/DELETE/:movie_id movie',()=>{
+        it('it should Delete a movie given by id',(done)=>{
+            chai.request(server)
+                .delete('/api/movies/'+movie_id)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('status').eql(1);
                     done();
                 });
         });
